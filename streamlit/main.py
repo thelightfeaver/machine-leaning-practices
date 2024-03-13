79ae58e7f8ee402bd573993c0a7d814ac24192db
thelightfeaver <diogenes_mj@hotmail.com>
2024-03-13 09:36:23 -0400

    feat: add streamlit for showing models

---
+++ streamlit/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+## Welcome to the landing page"
+st.title("Diogenes Monegro - DS")
+st.markdown("""
+Welcome to my personal website. I am a Data Scientist with a background in Economics and Finance.
+""")
---
+++ streamlit/pages/2_temp.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import joblib
+
+
+
+model = joblib.load('streamlit/model/temp.pkl')
+
+temperature = st.sidebar.slider('Temperature', 0, 100, 25)
+
+prediction = model.predict([[temperature]])
+
+st.title(f'The current temperature is {temperature}°C 🌡️')
+st.markdown(f"""
+    ### Convertion {round(prediction[0][0]) } °H
+""")
+
+
+
